d85ba4c291248cd24040e5aeaf69bf440ee5ac23
iYaktina <168226966+iYaktina@users.noreply.github.com>
2024-06-23 19:28:25 +0300

    Testing purpose

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-	// Function to extract parameters from URL
 	function getUrlParameter(name) {
 		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
 		var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
@@ -9,34 +8,29 @@ $(document).ready(function () {
 			: decodeURIComponent(results[1].replace(/\+/g, " "));
 	}
 
-	// Extract car name and price from URL
 	var carName = getUrlParameter("name");
 	var carPrice = getUrlParameter("price");
 	var carDesc = getUrlParameter("description");
 	var carYear = getUrlParameter("year");
 	var carColor = getUrlParameter("color");
-	// Display car name and price on the payment page
 	$("#carName").text(carName);
 	$("#carPrice").text(carPrice);
 	$("#carYear").text(carYear);
 	$("#carColor").text(carColor);
 	$("#carDescription").text(carDesc);
 	$("#payButton").click(function () {
-		// Validate card holder
 		var cardHolder = $(".card_holder").val().trim();
 		if (cardHolder === "") {
 			alert("Please enter the card holder name.");
 			return;
 		}
 
-		// Validate card number
 		var cardNumber = $(".card_number").val().replace(/\s/g, "");
 		if (!/^\d{16}$/.test(cardNumber)) {
 			alert("Please enter a valid 16-digit credit card number.");
 			return;
 		}
 
-		// Validate expiry date (MM/YY)
 		var expiryDate = $(".expiry_date").val().trim();
 		var today = new Date();
 		var currentMonth = today.getMonth() + 1;
@@ -63,20 +57,16 @@ $(document).ready(function () {
 });
 
 function getSubstringUntilFirstWhitespace(inputString) {
-	// Check if inputString is defined and is a string
 	if (typeof inputString !== "string") {
 		return "";
 	}
 
-	// Find the index of the first whitespace
 	const firstWhitespaceIndex = inputString.indexOf(" ");
 
-	// If no whitespace is found, return the whole string
 	if (firstWhitespaceIndex === -1) {
 		return inputString;
 	}
 
-	// Return the substring from the start to the first whitespace
 	return inputString.substring(0, firstWhitespaceIndex);
 }
 
@@ -119,6 +109,6 @@ document
 				console.error("Error making Order:", error);
 				alert(
 					"An error occurred while placing your Order information."
-				); // General error message for the user
+				); 
 			});
 	});
