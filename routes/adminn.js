1bd088cf3ead236d0a7267d6dc4e3328cc858cb4
iYaktina <168226966+iYaktina@users.noreply.github.com>
2024-06-24 00:14:54 +0300

    File manag

--- routes/adminn.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.admin-container');
-    sections.forEach(section => {
-        section.classList.remove('active');
-    });
-    document.getElementById(sectionId).classList.add('active');
-}
-
-
