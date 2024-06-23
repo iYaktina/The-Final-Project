9b83606ec725c86e51d92c1e99365b1f4573d0e1
iYaktina <168226966+iYaktina@users.noreply.github.com>
2024-06-23 19:28:08 +0300

    Order info

--- models/User.js
+++ models/User.js
@@ -68,18 +68,4 @@ const UserSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("User", UserSchema);
-/*
-// Hash the password before saving the user
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
 
-UserSchema.methods.isValidPassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-};
-*/
